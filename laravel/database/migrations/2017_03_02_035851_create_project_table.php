c2420db1d96d6be499490e2c4eff2cf66201d369
Chu Tien Dien <dienctsimplelife@gmail.com>
2017-03-02 11:41:54 +0700

    add new table project and channel

---
+++ laravel/database/migrations/2017_03_02_035851_create_project_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 200)->unique()->comment = "Tên Project";
+            $table->string('description');
+            $table->unsignedTinyInteger('status')->default(1)->comment = "Trạng thái 0 và 1";            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
---
+++ laravel/database/migrations/2017_03_02_040203_create_channel_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChannelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channel', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 200)->unique()->comment = "Tên Kenh";
+            $table->string('description');
+            $table->unsignedTinyInteger('status')->default(1)->comment = "Trạng thái 0 và 1";            
+            $table->unsignedInteger('amdin_modify');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channel');
+    }
+}
