6997842d76b469551cc1dd1c2d8df16f1de7c867
dienchutien <dienct@dxmb.vn>
2017-03-04 15:58:37 +0700

    table user cmmit

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email',100)->unique();
             $table->unsignedTinyInteger('status')->default(1)->comment = "Trạng thái 0 và 1";
             $table->string('password');
             $table->rememberToken()->comment = "Token ghi nhớ mỗi lần đăng nhập";
